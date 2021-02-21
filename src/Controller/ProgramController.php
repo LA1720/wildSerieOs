316c8d3565cae1ec1b6703f8dfec7845fc9eefcd
la1720 <layaafzal74@gmail.com>
2021-02-21 02:35:55 +0100

    routing, wildcard

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -6,15 +6,29 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+/**
+ * Undocumented class
+ * @Route("/program", name="program_")
+ */
 class ProgramController extends AbstractController
 {
     /**
-     * @Route("/programs/", name="program_index")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
         return $this->render('program/index.html.twig', [
             'website' => 'Wild Serie'
+        ]);
+    }
+
+    /**
+     * @Route("/show/{page}", methods={"GET"}, requirements={"page"="\d+"}, name="show")
+     */
+    public function show(int $page): Response
+    {
+        return $this-> render('program/show.html.twig', [
+            'page' => $page
         ]);
     }
 }
